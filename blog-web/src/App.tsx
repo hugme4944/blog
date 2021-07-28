af7e3d600a0ad90ce9797b8119810bd83dc05279
hugme4944 <hugme4944@gmail.com>
2021-07-28 12:01:39 +0900

    Card 컴포넌트 생성

--- blog-web/src/App.tsx
+++ blog-web/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Card } from "./components/Card";
 
 function App() {
   return (
     <div>
       <h1>Blog</h1>
       <p>My Blog Service</p>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
---
+++ blog-web/src/components/Card.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Card() {
+  return <div>블로그 포스트</div>;
+}
