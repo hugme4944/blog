1823bf8f31deeb5c209248452ebe612d447866e6
hugme4944 <hugme4944@gmail.com>
2021-07-28 16:12:26 +0900

    ArticleAddPage 추가

--- blog-web/src/App.tsx
+++ blog-web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ArticleAddPage } from "./pages/ArticleAddPage";
 import { ArticlePage } from "./pages/ArticlePage";
 import { HomePage } from "./pages/HomePage";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/articles/add" component={ArticleAddPage} />
         <Route path="/articles/:id" component={ArticlePage} />
         <Route path="/" component={HomePage} />
       </Switch>
