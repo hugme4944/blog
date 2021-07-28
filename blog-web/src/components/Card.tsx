dd19a422a6c9900448bb7f999534e979a4c6c898
hugme4944 <hugme4944@gmail.com>
2021-07-28 14:46:41 +0900

    스타일 추가

--- blog-web/src/components/Card.tsx
+++ blog-web/src/components/Card.tsx
@@ -3,8 +3,16 @@ import { FC } from "react";
 
 interface CardProps {
   title: string;
+  content: string;
+  thumbnail: string;
 }
 
-export const Card: FC<CardProps> = ({ title }) => {
-  return <div>{title}</div>;
+export const Card: FC<CardProps> = ({ title, content, thumbnail }) => {
+  return (
+    <button className="text-left">
+      <img src={thumbnail} alt="thumbnail" />
+      <p className="text-gray-800 text-xl">{title}</p>
+      <p className="text-gray-500 text-sm truncate">{content}</p>
+    </button>
+  );
 };
