03f27696f259a8c811dfe0ea6d0c874e7881a0bc
hugme4944 <hugme4944@gmail.com>
2021-08-05 10:28:45 +0900

    users 모듈 생성

--- blog-api/src/app.module.ts
+++ blog-api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticlesModule } from './articles/articles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ArticlesModule,
+    UsersModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'temp.db',
